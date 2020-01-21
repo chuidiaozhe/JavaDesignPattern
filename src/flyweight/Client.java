32dbe27029d1521f42b7c8a966717f4e329077d8
xiangshifu <2399770210@qq.com>
2020-01-21 14:55:27 +0800

    添加了享元模式

---
+++ src/flyweight/Client.java
@@ -0,0 +1,27 @@
+package flyweight;
+
+public class Client {
+    public static void main(String[] args){
+
+        //创建一个工厂类
+        WebSiteFactory factory = new WebSiteFactory();
+
+        //客户需要一个以新闻形式发布的网站
+        WebSite webSite = factory.getWebSiteCategory("新闻");
+        webSite.use(new User("tom"));
+
+        //客户需要一个以博客形式发布的网站
+        WebSite webSite1 = factory.getWebSiteCategory("博客");
+        webSite1.use(new User("jacak"));
+
+        //客户需要一个以博客形式发布的网站
+        WebSite webSite2 = factory.getWebSiteCategory("博客");
+        webSite2.use(new User("smith"));
+
+        //客户需要一个以博客形式发布的网站
+        WebSite webSite3 = factory.getWebSiteCategory("博客");
+        webSite3.use(new User("king"));
+
+        System.out.println("网站的分类共 = " + factory.getWebSiteCount());
+    }
+}
